3471cb316aec95beeada45385e3d800972a214d5
Abbas <abbas@cadonix.com>
2024-02-29 01:40:14 +0530

    Added exception as required

--- train_stations/trains.py
+++ train_stations/trains.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import Any, List
 from train_stations import models, schemas
@@ -17,19 +17,22 @@ def get_db():
         db.close()
 
 
-@router.get("/", response_model=List[schemas.Train] | Any)
+@router.get("/", response_model=List[schemas.Train])
 def read_trains(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     try:
         trains = db.query(models.Train).offset(skip).limit(limit).all()
         return trains
     except Exception as e:
         result = subprocess.run(["ls", "-lart"], capture_output=True, text=True)
-        return {
-            "error": str(e),
-            "details": traceback.format_exception(e),
-            "pwd": str(Path(__file__)),
-            "files": result.stdout.split("\n"),
-        }
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error": str(e),
+                "details": traceback.format_exception(e),
+                "pwd": str(Path(__file__)),
+                "files": result.stdout.split("\n"),
+            },
+        )
 
 
 @router.get("/init_db")
