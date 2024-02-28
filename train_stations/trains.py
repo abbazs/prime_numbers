0a07c8fe97dd31ce26fd3ca1c009aec082a33070
Abbas <abbas@cadonix.com>
2024-02-29 01:12:03 +0530

    Removed update and create routes

--- train_stations/trains.py
+++ train_stations/trains.py
@@ -15,26 +15,6 @@ def get_db():
         db.close()
 
 
-@router.post("/", response_model=schemas.Train)
-def create_train(train: schemas.TrainCreate, db: Session = Depends(get_db)):
-    db_train = models.Train(**train.model_dump())
-    db.add(db_train)
-    db.commit()
-    db.refresh(db_train)
-    return db_train
-
-
-@router.put("/{train_id}", response_model=schemas.Train)
-def update_train(train_id: int, train: schemas.Train, db: Session = Depends(get_db)):
-    db_train = db.query(models.Train).filter(models.Train.id == train_id).first()
-    if not db_train:
-        raise HTTPException(status_code=404, detail="Train not found")
-    for key, value in train.model_dump().items():
-        setattr(db_train, key, value)
-    db.commit()
-    return db_train
-
-
 @router.get("/", response_model=List[schemas.Train])
 def read_trains(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     trains = db.query(models.Train).offset(skip).limit(limit).all()
